5ed2bdda61283b0311f68e08bd39a88d703378aa
Jxlux <openworld9102@gmail.com>
2024-08-26 21:06:01 -0400

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,23 @@
+# imports turtle
+# imported as t, must use t. before instead of turtle
+
+from random import random
+import turtle as t
+
+
+t.setpos((20,80))
+
+t.forward(100)
+
+t.right(50)
+
+t.forward(100)
+
+t.right(50)
+
+t.forward(100)
+
+t.circle(50)
+
+t.mainloop()
+
